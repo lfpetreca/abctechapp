4e032738010f53e3738a4ebbacd828e71ceb1cb2
Luis Fernando M. Petreca <lfpetreca02@gmail.com>
2022-10-13 21:15:10 -0300

    Seleção de assistencias

--- lib/controller/assist_controller.dart
+++ lib/controller/assist_controller.dart
@@ -5,20 +5,53 @@ import '../services/assist_services.dart';
 
 class AssistController extends GetxController with StateMixin<List<Assist>> {
   late AssistService _service;
+  List<Assist> allAssists = [];
+  List<Assist> selectedAssists = [];
 
   @override
   void onInit() {
     super.onInit();
-    change([], status: RxStatus.empty());
     _service = Get.find<AssistService>();
+    getAssistList();
   }
 
   void getAssistList() {
     change([], status: RxStatus.loading());
-    _service
-        .getAssists()
-        .then((value) => change(value, status: RxStatus.success()))
-        .onError((error, stackTrace) =>
-            change([], status: RxStatus.error(error.toString())));
+    _service.getAssists().then((value) {
+      allAssists = value;
+      change(value, status: RxStatus.success());
+    }).onError((error, stackTrace) {
+      change([], status: RxStatus.error(error.toString()));
+    });
+  }
+
+  void finishSelectAssists() {
+    Get.back();
+  }
+
+  void selectList(int index) {
+    Assist assist = allAssists[index];
+    int indexFound =
+        selectedAssists.indexWhere((element) => element.id == assist.id);
+
+    if (indexFound == -1) {
+      selectedAssists.add(assist);
+    } else {
+      selectedAssists.removeAt(indexFound);
+    }
+
+    change(allAssists, status: RxStatus.success());
+  }
+
+  bool isSelected(int index) {
+    Assist assist = allAssists[index];
+    int indexFound =
+        selectedAssists.indexWhere((element) => element.id == assist.id);
+
+    if (indexFound >= 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,41 +11,57 @@ class HomePage extends GetView<AssistController> {
     return ListView.builder(
       shrinkWrap: true,
       itemCount: assist.length,
-      itemBuilder: ((context, index) =>
-          ListTile(title: Text(assist[index].name))),
+      itemBuilder: ((context, index) => ListTile(
+            title: Text(assist[index].name),
+            selectedColor: Colors.red,
+            selected: controller.isSelected(index),
+            onTap: () => controller.selectList(index),
+          )),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Lista de Serviços')),
-        body: Container(
-          constraints: const BoxConstraints.expand(),
+      appBar: AppBar(title: const Text('Lista de Serviços')),
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Row(
                 children: const [
                   Expanded(
-                      child: Text('Os serviços disponíveis são:',
-                          textAlign: TextAlign.center))
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                        onPressed: controller.getAssistList,
-                        child: const Text('Regarregar')),
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        'Os serviços disponíveis são:',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ),
-              controller.obx((state) => renderAssits(state ?? []),
-                  onEmpty: const Text('Não encontramos nenhum serviço.'),
-                  onError: (error) => Text(error.toString()))
+              controller.obx(
+                (state) => renderAssits(state ?? []),
+                onEmpty: const Text('Não há assistências disponíveis.'),
+                onError: (error) => Text(error.toString()),
+              ),
             ],
           ),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.finishSelectAssists();
+        },
+        child: const Icon(Icons.done),
+      ),
+    );
   }
 }
